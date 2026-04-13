c334e9296ef3baa36d5a0bec62a841a1d7358088
Alvaro Cadenas <acadenas@stemdo.io>
2026-04-13 10:02:27 +0200

    Componente ScrollToTop añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRouter from "./router/AppRouter";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <div className="app-wrapper">
+      <ScrollToTop />
       <Navbar />
 
       <main className="main-content">
